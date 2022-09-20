75348b4968f09cdd4391d5fdc4f18c0b0d02a3cc
Pablo Henrique Castelo <pablohenriquec7@gmail.com>
2022-09-20 19:36:59 -0300

    ajuste do cors no server, inicio do desenvolvimento do front-end e GET dos links na API

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
 const saveMyLinksRoutes = require('./saveMyLinks.routes')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(saveMyLinksRoutes)
 
 app.get('/health', (req, res) => {
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,58 @@
+import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material'
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 export const App = () => {
-  return <></>
+  const [links, setLinks] = useState([])
+
+  //Carrega a API ao renderizar a página
+  useEffect(() => {
+    getLinks()
+  }, [])
+
+  //GET da API que retorna um array de links salvos
+  const getLinks = async () => {
+    const endpoint = 'http://localhost:3333/links'
+
+    const response = await axios.get(endpoint)
+    setLinks(response.data)
+    console.log(links)
+  }
+
+  return (
+    <>
+      <Box sx={{ backgroundColor: '#e6e6e6', height: '100vh' }}>
+        {/* HEADER */}
+        <Box sx={{ flexGrow: 1, mb: '1rem' }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                SaveMyLink
+              </Typography>
+              <Typography>
+                <AccountCircleIcon />
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+
+        {/* CONTENT */}
+        <Container maxWidth="sm">
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <Typography variant="h3" sx={{ mb: '1rem' }}>
+              Bem vindo!
+            </Typography>
+          </Box>
+        </Container>
+      </Box>
+    </>
+  )
 }
