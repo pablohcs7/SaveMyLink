27dd494d6a41dec325d422edc8e47a70ad2d98e7
Pablo Henrique Castelo <pablohenriquec7@gmail.com>
2022-09-24 15:40:21 -0300

    ajustes para deploy do server

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,8 @@ const saveMyLinksRoutes = require('./saveMyLinks.routes')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.use(express.json())
 app.use(cors())
 app.use(saveMyLinksRoutes)
@@ -13,4 +15,4 @@ app.get('/health', (req, res) => {
   return res.json('up')
 })
 
-app.listen(3333, () => console.log('Server up in 3333'))
+app.listen(port, () => console.log('Server up in 3333'))
